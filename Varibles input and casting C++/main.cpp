9a4a8df7d444073255df77943aaa22fc2b7f8427
Bailey Borchers <baileyborchers@gmail.com>
2021-06-26 16:37:31 -0700

    Initial Commit

---
+++ Varibles input and casting C++/main.cpp
@@ -0,0 +1,14 @@
+//
+//  main.cpp
+//  Varibles input and casting C++
+//
+//  Created by Bailey Borchers on 6/26/21.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
