3928a2671b6d1c884f419b896d04219229253d30
Bailey Borchers <baileyborchers@gmail.com>
2021-06-26 16:45:17 -0700

    Attempt 1

--- Varibles input and casting C++/main.cpp
+++ Varibles input and casting C++/main.cpp
@@ -6,9 +6,41 @@
 //
 
 #include <iostream>
+#include <string>      // Supports use of "string" data type
+using namespace std;
 
-int main(int argc, const char * argv[]) {
-    // insert code here...
-    std::cout << "Hello, World!\n";
-    return 0;
+int main() {
+    int    userInt    = 0;
+    double userDouble = 0.0;
+    char   userChar;
+    string userString;
+   
+    cout << "Enter integer:" << endl;
+    cin >> userInt;
+    cout << endl;
+    cout << "Enter double:";
+    cout << endl;
+    cin >> userDouble;
+    cout << endl;
+    cout << "Enter character:";
+    cout << endl;
+    cin >> userChar;
+    cout << endl;
+    cout << "Enter string:";
+    cout << endl;
+    cin >> userString ;
+    cout << endl;
+    cout << userInt << " " << userDouble << " " << userChar << " " << userString << endl;
+    
+    
+   
+   // FIXME (1): Finish reading other items into variables, then output the four values on a single line separated by a space
+   
+   
+   // FIXME (2): Output the four values in reverse
+   
+   
+   // FIXME (3): Cast the double to an integer, and output that integer
+
+   return 0;
 }
